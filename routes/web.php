64ec7addc05d37bddfa3337872efccb0b99ae764
AnastasiaDolgopolova <anastasiadolgopolova11@gmail.com>
2024-11-22 12:17:26 +0400

    add sanctum

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Auth::routes();
+
+Route::get('/{page}', [App\Http\Controllers\MainController::class, 'index'])->where('page', '.*');
